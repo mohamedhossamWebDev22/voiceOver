d37385092d2f43f4c8933d5dd1b651c915e231a5
Malek mohamed mariam <mohamedhossamelden2009@gmail.com>
2022-06-16 15:56:24 +0200

    coded and fixed

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import tkinter
 from gtts import gTTS
 from tkinter import *
 import os
@@ -10,21 +9,24 @@ window.geometry("500x400")
 txt = Label(window, width=500, text="Arabic voice over", bg= "blue", fg= "white", font=("sans-serif", 40))
 txt.pack()
 
-inp = Entry(window, width=20, font=("sans-serif", 30))
-inp.place(bordermode=INSIDE, y= 100, x= 25)
+l = Label(window, width=500, text="", bg= "blue", fg= "white", font=("sans-serif", 15))
+l.pack()
 
-klam = str(inp.get())
+inpt = Entry(window, width=20, font=("sans-serif", 30))
+inpt.place(bordermode=INSIDE, y= 100, x= 25)
 
+def go():
+    klam = str(inpt.get())
+    robot = gTTS(text = klam, lang='ar', slow=False)
 
-
-def voice():
-    robot = gTTS(klam, lang='ar', slow=False)
+    l.configure(text=klam)
+    # print(klam)
     
     robot.save('go.mp3')
-    os.system("go.mp3")
+    os.system('go.mp3')
     
 
-btn = Button(window, text="Go", bg= "blue",fg= "white", font=("sans-serif", 20), command= voice())
+btn = Button(window, text="Go", bg= "blue",fg= "white", font=("sans-serif", 20), command= go)
 btn.place(bordermode=INSIDE, y= 200, x = 200)
 
 window.mainloop()
