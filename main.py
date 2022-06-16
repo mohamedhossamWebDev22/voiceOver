4cb7cff6f5a367f43a33970353daf95d7fd5eac7
Malek mohamed mariam <mohamedhossamelden2009@gmail.com>
2022-06-16 16:13:15 +0200

    Added my name

--- main.py
+++ main.py
@@ -15,12 +15,12 @@ l.pack()
 inpt = Entry(window, width=20, font=("sans-serif", 30))
 inpt.place(bordermode=INSIDE, y= 100, x= 25)
 
+#robot telling the klam.
 def go():
     klam = str(inpt.get())
     robot = gTTS(text = klam, lang='ar', slow=False)
 
     l.configure(text=klam)
-    # print(klam)
     
     robot.save('go.mp3')
     os.system('go.mp3')
@@ -28,5 +28,11 @@ def go():
 
 btn = Button(window, text="Go", bg= "blue",fg= "white", font=("sans-serif", 20), command= go)
 btn.place(bordermode=INSIDE, y= 200, x = 200)
+
+made = '''Made by
+    محمد حسام الدين محمد'''
+
+who = Label(window, text= made, width=70,bg= "blue", fg= "white")
+who.place(bordermode=OUTSIDE, y=350, x = 0)
 
 window.mainloop()
